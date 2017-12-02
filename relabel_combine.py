ed986d577d56cb687ed5b8016e7331d7acb9d2e9
Shiyin Jiao <sjiao@uchicago.edu>
2017-12-01 19:52:25 -0600

    Update and rename relabel.py to relabel_combine.py

--- relabel_combine.py
+++ relabel_combine.py
@@ -2,18 +2,21 @@
 
 import pandas as pd
 
-for year in range(2012, 2017):
+insurance_combined = pd.DataFrame()
+for year in range(2012,2017):
     filename = "acs_" + str(year) + ".csv"
-    insurance_df_year = pd.read_csv("data/" + filename, index_col = ["county"])
-    insurance_df_year.rename(columns = {"NAME":"County, State", "state":"State FIPS",
+    insurance_year = pd.read_csv("data/" + filename, index_col = ["county"])
+    insurance_year.rename(columns = {"NAME":"County, State", "state":"State FIPS",
         #age
         "DP05_0004PE":"Under 5 years", "DP05_0005PE":"5 to 9 years", "DP05_0006PE":"10 to 14 years", 
         "DP05_0007PE":"15 to 19 years", "DP05_0008PE":"20 to 24 years", "DP05_0009PE":"25 to 34 years", 
         "DP05_0010PE":"35 to 44 years", "DP05_0011PE":"45 to 54 years", "DP05_0012PE":"55 to 59 years",
-        "DP05_0013PE":"60 to 64 years", "DP05_0021PE":"65 years and over",
-        #sex, race, employment status
+        "DP05_0013PE":"60 to 64 years", "DP05_0021PE":"65 years and over", "DP05_0018PE":"18 years and over",
+        #sex, 
+        #race (values missing for 57,74,49 counties in 2012,13,14, won't use in regression), 
+        #employment status
         "DP05_0002PE":"Male", "DP05_0072PE":"Non-Hispanic White", "DP05_0073PE":"Non-Hispanic Black", 
-        "DP05_0066PE":"Hispanic or Latino", "DP03_0001PE":"Unemployed (>=16 years,In civilian labor force)",
+        "DP05_0066PE":"Hispanic or Latino", "DP03_0005PE":"Unemployed (>=16 years,In civilian labor force)",
         #income
         "DP03_0062E":"Median household income", "DP03_0052PE":"Less than $10,000", "DP03_0053PE":"$10,000 to $14,999", 
         "DP03_0054PE":"$15,000 to $24,999", "DP03_0055PE":"$25,000 to $34,999", "DP03_0056PE":"$35,000 to $49,999", 
@@ -31,4 +34,23 @@ for year in range(2012, 2017):
         "DP03_0113PE":"18-64, Unmployed, No insurance", "DP03_0116PE":"18-64, Not in labor force, With private insurance", 
         "DP03_0117PE":"18-64, Not in labor force, With public insurance", "DP03_0118PE":"18-64, Not in labor force, No insurance"
         }, inplace = True)
-    insurance_df_year.drop('Unnamed: 0', axis=1, inplace=True)
+    insurance_year.drop('Unnamed: 0', axis=1, inplace=True)
+    insurance_year["Year"] = year
+    
+    #insurance_norace_year = insurance_year[insurance_year["Non-Hispanic White"]=="N"]
+    #print(len(insurance_norace_year))
+    
+    # Re-Categorize
+    insurance_year["Under 18 years"] = 100 - insurance_year["18 years and over"]
+    insurance_year["18 - 64 years"] = insurance_year["18 years and over"] - insurance_year["65 years and over"]
+    insurance_year["< High school"] = insurance_year["Less than 9th grade"] + insurance_year["9th to 12th grade, no diploma"]
+    insurance_year["Associate's/Bachelor's degree"] = insurance_year["Associate's degree"] + insurance_year["Bachelor's degree"]
+    insurance_year = insurance_year.apply(pd.to_numeric, errors='coerce')
+        
+    insurance_combined = insurance_combined.append(insurance_year)
+
+insurance_2012 = insurance_combined[insurance_combined["Year"] == 2012]
+insurance_2013 = insurance_combined[insurance_combined["Year"] == 2013]
+insurance_2014 = insurance_combined[insurance_combined["Year"] == 2014]
+insurance_2015 = insurance_combined[insurance_combined["Year"] == 2015]
+insurance_2016 = insurance_combined[insurance_combined["Year"] == 2016]
